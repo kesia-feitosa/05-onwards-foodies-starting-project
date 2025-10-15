7420758f02f8d960323cbf11b7c09a073eddb127
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-10-15 11:17:10 -0400

    Add suspense feature to show loading data

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,16 @@ import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals(params) {
+    const meals = await getMeals();
+    
+    return <MealsGrid meals={meals} />
+}
 
 export default async function MealsPage() {
-    const meals = await getMeals();
+    
 
     return (
       <>
@@ -24,7 +31,9 @@ export default async function MealsPage() {
 
         </header>
         <main className={classes.main}>
-            <MealsGrid meals={meals} />
+          <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+            <Meals />
+          </Suspense>
         </main>
       </>
     );

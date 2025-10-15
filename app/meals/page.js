facd88beb540be9f2bce714df290b1b9f9fffa60
Keke <67804511+kesia-feitosa@users.noreply.github.com>
2025-10-15 10:18:13 -0400

    Add meals component and database sqlite

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,31 @@
-export default function MealsPage() {
-    return <h1>Meals Page</h1>
-}
+import MealsGrid from '@/components/meals/meals-grid';
+import classes from './page.module.css';
+import Link from 'next/link';
+import { getMeals } from '@/lib/meals';
+
+export default async function MealsPage() {
+    const meals = await getMeals();
+
+    return (
+      <>
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created{' '}
+                <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>
+                Choose your favorite recipe and cook it yourself. It is easy and fun!
+            </p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share your Favorite Recipe
+                </Link>
+            </p>
+
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={meals} />
+        </main>
+      </>
+    );
+} 
